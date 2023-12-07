678c6a1ebe683411e5c577784e307abceb83a81a
Wiqiro <william.imbert@utbm.fr>
2023-12-07 18:30:51 +0100

    feat: models for charbon and course types

--- src/models/Charbon.ts
+++ src/models/Charbon.ts
@@ -1,21 +1,19 @@
 import { Time } from "@angular/common";
 
-export class CardStyle {
+export class Charbon {
     constructor(
-        public style: string,
-        public ue: string,
+        public course: string,
         public date: string,
         public time: string,
         public title: string,
-        public actionner: string[],
+        public actionners: string[],
         public description: string,
     ) {
-        this.style = style,
-        this.ue = ue,
+        this.course = course,
         this.date = date,
         this.time = time,
         this.title = title,
-        this.actionner = actionner,
+        this.actionners = actionners,
         this.description = description
     }
 }
