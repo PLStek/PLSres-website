b9f5d9215eb7ba52b2c88c5a1fba1fe3aed52b97
WBiome <WorkBiome@proton.me>
2023-12-07 20:32:51 +0100

    navBar update for the burger menu

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
+
+  isToggled = false;
+
+  toggleClasses() {
+    this.isToggled = !this.isToggled;
+  }
 
   constructor() { }
 
