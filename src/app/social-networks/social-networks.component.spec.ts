d392d37d9b0eba0f4bf1a52dcbe23bddfbf94c23
Wiqiro <william.imbert@utbm.fr>
2023-12-08 08:22:33 +0100

    feat: social networks card

---
+++ src/app/social-networks/social-networks.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SocialNetworksComponent } from './social-networks.component';
+
+describe('SocialNetworksComponent', () => {
+  let component: SocialNetworksComponent;
+  let fixture: ComponentFixture<SocialNetworksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SocialNetworksComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SocialNetworksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
