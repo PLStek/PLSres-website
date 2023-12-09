f95e5f6eca76f7b3db1718de000ee82121f8cff0
Lucas <hublartlucas@gmail.com>
2023-12-09 20:57:51 +0100

    les events le dimanche ne fonctionne pas ? jsp pk

--- src/app/charbon.service.ts
+++ src/app/charbon.service.ts
@@ -15,6 +15,17 @@ export class CharbonService {
       new Charbon(
         'PM1',
         CourseTypes.elec,
+        '20/12/2023',
+        '20h00',
+        'AAP 4, 5 et 6 de PM1 !',
+        ['Lilit, Flo, William'],
+        'On prépare le CC2 de PM en révisant les AAP 4, 5 et 6. Nous utiliserons l’ECA du CC2 en distanciel A22, alors jetez-y un coup d’oeil !'
+      )
+    );
+    charbonList.push(
+      new Charbon(
+        'PM1',
+        CourseTypes.elec,
         '10/12/2023',
         '20h00',
         'AAP 4, 5 et 6 de PM1 !',
