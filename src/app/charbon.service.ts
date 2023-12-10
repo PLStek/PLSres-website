dc49232b4749d231910a61bd9684afc304a5c2fa
Wiqiro <william.imbert@utbm.fr>
2023-12-10 22:09:59 +0100

    feat: working time interval pipe

--- src/app/charbon.service.ts
+++ src/app/charbon.service.ts
@@ -16,7 +16,7 @@ export class CharbonService {
         1,
         'PM1',
         CourseTypes.elec,
-        new Date(2023, 11, 12, 15,0),
+        new Date(2023, 11, 12, 22,0),
         'AAP 4, 5 et 6 de PM1 !',
         ['Lilit, Flo, William'],
         'On prépare le CC2 de PM en révisant les AAP 4, 5 et 6. Nous utiliserons l’ECA du CC2 en distanciel A22, alors jetez-y un coup d’oeil !'
@@ -27,7 +27,7 @@ export class CharbonService {
         2,
         'PM1',
         CourseTypes.elec,
-        new Date(2023, 11, 12, 16,0),
+        new Date(2023, 11, 10, 23,0),
         'AAP 4, 5 et 6 de PM1 !',
         ['Lilit, Flo, William'],
         'On prépare le CC2 de PM en révisant les AAP 4, 5 et 6. Nous utiliserons l’ECA du CC2 en distanciel A22, alors jetez-y un coup d’oeil !'
--- src/app/date-interval.pipe.ts
+++ src/app/date-interval.pipe.ts
@@ -6,29 +6,37 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateIntervalPipe implements PipeTransform {
   transform(date: Date): string {
     const now = new Date();
-    const timeDifferenceInMilliseconds = date.getTime() - now.getTime();
-    const timeDifferenceInSeconds = timeDifferenceInMilliseconds / 1000;
-    const timeDifferenceInMinutes = timeDifferenceInSeconds / 60;
-    const timeDifferenceInHours = timeDifferenceInMinutes / 60;
-    const timeDifferenceInDays = timeDifferenceInHours / 24;
-    const timeDifferenceInWeeks = timeDifferenceInDays / 7;
+    const diffInMilliseconds = date.getTime() - now.getTime();
+    const diffInMinutes = Math.abs(diffInMilliseconds / 60000);
+    const diffInHours = diffInMinutes / 60;
 
-    if (timeDifferenceInWeeks >= 1) {
-      const numberOfWeeks = Math.floor(timeDifferenceInWeeks);
-      return `Dans ${numberOfWeeks} semaine${
-        numberOfWeeks > 1 ? 's' : ''
-      }`;
-    } else if (timeDifferenceInDays >= 2) {
-      const nombreDeJours = Math.floor(timeDifferenceInDays);
-      return `Il y a ${nombreDeJours} jour${nombreDeJours > 1 ? 's' : ''}`;
-    } else if (timeDifferenceInDays >= 1) {
-      return 'Hier';
-    } else if (timeDifferenceInHours >= 1) {
-      const numberOfHours = Math.floor(timeDifferenceInHours);
-      return `Dans ${numberOfHours} heure${numberOfHours > 1 ? 's' : ''}`;
+    // Getting the difference in days by getting rid of the time of day
+    now.setHours(0, 0, 0, 0);
+    date.setHours(0, 0, 0, 0);
+    const diffInDays = Math.abs(
+      (date.getTime() - now.getTime()) / (1000 * 60 * 60 * 24)
+    );
+
+    const diffInWeeks = diffInDays / 7;
+
+    let timeString = diffInMilliseconds < 0 ? 'Il y a ' : 'Dans ';
+
+    if (diffInWeeks >= 1) {
+      const weeks = Math.floor(diffInWeeks);
+      timeString += weeks + ' semaine' + (weeks > 1 ? 's' : '');
+    } else if (diffInDays >= 2) {
+      const days = Math.floor(diffInDays);
+      timeString += days + ' jours';
+    } else if (diffInDays >= 1) {
+      timeString = diffInMilliseconds < 0 ? 'Hier' : 'Demain';
+    } else if (diffInHours >= 1) {
+      const hours = Math.floor(diffInHours);
+      timeString += hours + ' heure' + (hours > 1 ? 's' : '');
     } else {
-      const numberOfMinutes = Math.floor(timeDifferenceInHours);
-      return `Dans ${numberOfMinutes} minute${numberOfMinutes > 1 ? 's' : ''}`;
+      const minutes = Math.floor(diffInMinutes);
+      timeString += minutes + ' minute' + (minutes > 1 ? 's' : '');
     }
+
+    return timeString;
   }
 }
