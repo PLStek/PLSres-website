e050af548bb14d6fbf302abc6e22359071fe4f83
WBiome <WorkBiome@proton.me>
2023-12-10 19:49:34 +0100

    add : exercise component

---
+++ src/app/exercise/exercise.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Charbon } from 'src/models/Charbon';
+
+@Component({
+  selector: 'app-exercise',
+  templateUrl: './exercise.component.html',
+  styleUrls: ['./exercise.component.scss']
+})
+export class ExerciseComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
