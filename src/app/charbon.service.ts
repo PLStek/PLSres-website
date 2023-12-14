6f0f2c7a1dcee575910ae101a85a1e7669ca9887
WBiome <WorkBiome@proton.me>
2023-12-14 12:06:39 +0100

    style: center elements

--- src/app/charbon.service.ts
+++ src/app/charbon.service.ts
@@ -55,6 +55,17 @@ export class CharbonService {
         'On prépare le CC2 de PM en révisant les AAP 4, 5 et 6. Nous utiliserons l’ECA du CC2 en distanciel A22, alors jetez-y un coup d’oeil !'
       )
     );
+    charbonList.push(
+      new Charbon(
+        4,
+        'IF2',
+        CourseType.info,
+        new Date(2023, 11, 12, 20,0),
+        'AAP 4, 5 et 6 de PM1 !',
+        ['Lilit, Flo, William'],
+        'On prépare le CC2 de PM en révisant les AAP 4, 5 et 6. Nous utiliserons l’ECA du CC2 en distanciel A22, alors jetez-y un coup d’oeil !'
+      )
+    );
     charbonList.sort((a: Charbon, b: Charbon) => {
       const dateA = new Date(a.date);
       const dateB = new Date(b.date);
