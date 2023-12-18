87cae4bfb2ee577dd20879c2a6764cf93211ce81
Lucas <hublartlucas@gmail.com>
2023-12-18 23:20:32 +0100

    add annoucement

--- src/app/announcements/announcement-card/announcement-card.component.ts
+++ src/app/announcements/announcement-card/announcement-card.component.ts
@@ -10,12 +10,19 @@ export class AnnouncementCardComponent implements OnInit {
   @Input() contentSource!: string;
   @Input() date!: Date;
   @Input() collapse?: boolean = false;
+  @Input() isEmpty?: boolean = false;
+
+  currentDate: string = new Date().toISOString().split('T')[0];
 
   constructor() {
     this.title;
     this.contentSource;
     this.date
   }
-
+  addAnnouncement(): void {
+    console.log(this.title);
+    console.log(this.contentSource);
+    console.log(this.date);
+  }
   ngOnInit(): void { }
 }
