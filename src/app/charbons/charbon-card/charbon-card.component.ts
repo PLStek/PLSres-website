92c17cc6ce13a20902beecff7a4c4865a6f42703
Lucas <hublartlucas@gmail.com>
2023-12-20 18:33:15 +0100

    form group edit

--- src/app/charbons/charbon-card/charbon-card.component.ts
+++ src/app/charbons/charbon-card/charbon-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { Charbon } from 'src/app/shared/models/charbon.model';
 import { CourseType } from 'src/app/shared/utils/course-type.model';
 import { ActionneurService } from 'src/app/shared/services/actionneur.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-charbon-card',
@@ -11,15 +12,30 @@ import { ActionneurService } from 'src/app/shared/services/actionneur.service';
 export class CharbonCardComponent implements OnChanges {
   @Input() charbon!: Charbon;
   @Input() edit: boolean = false;
+
+  form!: FormGroup;
   isEditing: boolean = false;
   editedCharbon!: Charbon;
   actionneurList: String[] = [];
 
-  constructor(private actionneurService: ActionneurService) {
+  constructor(
+    private actionneurService: ActionneurService,
+    private formBuilder: FormBuilder
+    ) {
     this.charbon;
     this.edit;
   }
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      title: '',
+      course: '',
+      courseType: CourseType.undefined,
+      date: '2023',
+      actionneurs: [],
+      description: '',
+      rediffusion:'',
+      ressources:''
+    });
     this.actionneurService.getActionneurs().subscribe((data) => {
       this.actionneurList = data;
     });
