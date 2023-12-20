b1d28de762d88ea0631eb4fc18228f6090f6cb0f
Wiqiro <william.imbert@utbm.fr>
2023-12-20 15:34:54 +0100

    feat: dynamic form for charbon addition

--- src/app/actionner/add-charbon/add-charbon.component.ts
+++ src/app/actionner/add-charbon/add-charbon.component.ts
@@ -1,6 +1,7 @@
 import { CoursesService } from './../../shared/services/courses.service';
 import { Time } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Charbon } from 'src/app/shared/models/charbon.model';
 import { Course } from 'src/app/shared/models/course.model';
 import { CharbonService } from 'src/app/shared/services/charbon.service';
@@ -13,8 +14,8 @@ import { CourseType } from 'src/app/shared/utils/course-type.model';
 })
 export class AddCharbonComponent implements OnInit {
   newCharbon!: Charbon;
-  dateInput: String = '2023';
-  timeInput: String = '00:00';
+  form!: FormGroup;
+
   courseList: Course[] = [];
   courseListForSelectedType: Course[] = [];
 
@@ -22,7 +23,8 @@ export class AddCharbonComponent implements OnInit {
 
   constructor(
     private charbonService: CharbonService,
-    private courseService: CoursesService
+    private courseService: CoursesService,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
@@ -30,29 +32,44 @@ export class AddCharbonComponent implements OnInit {
       0,
       '',
       CourseType.undefined,
-      new Date('2023'),
+      new Date('2020T20:20'),
       '',
       [],
       ''
     );
 
+    this.form = this.formBuilder.group({
+      title: '',
+      course: '',
+      courseType: CourseType.undefined,
+      date: '2020',
+      time: '20:20',
+      description: '',
+    });
+
     this.courseService.getCourses().subscribe((data) => {
       this.courseList = data;
     });
-  }
 
-  updateCourseListForType(): void {
-    console.log(this.newCharbon.courseType);
-    this.courseListForSelectedType = this.courseList.filter(
-      (course) => course.type === this.newCharbon.courseType
-    );
-    this.newCharbon.course = "";
-  }
+    this.form.valueChanges.subscribe((data) => {
+      this.newCharbon = new Charbon(
+        0,
+        data.course,
+        data.courseType,
+        new Date(data.date + 'T' + data.time),
+        data.title,
+        [],
+        data.description
+      );
+      console.log(data);
+    });
 
-  updateDate(): void {
-    const newDate = this.dateInput + 'T' + this.timeInput;
-    console.log(newDate);
-    this.newCharbon.date = new Date(newDate);
+    this.form.get('courseType')?.valueChanges.subscribe((data) => {
+      this.courseListForSelectedType = this.courseList.filter(
+        (course) => course.type === data
+      );
+      this.form.get('course')?.setValue('');
+    });
   }
 
   addCharbon(): void {
