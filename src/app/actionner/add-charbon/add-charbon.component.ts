0474916dd3ecb5c491b6c27e35d1f60295772f2a
Wiqiro <william.imbert@utbm.fr>
2023-12-21 14:11:54 +0100

    feat: fully working charbon addition

--- src/app/actionner/add-charbon/add-charbon.component.ts
+++ src/app/actionner/add-charbon/add-charbon.component.ts
@@ -1,9 +1,11 @@
+import { CharbonPostParameters } from './../../shared/models/charbon-post-parameters.model';
 import { CoursesService } from './../../shared/services/courses.service';
 import { Time } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Charbon } from 'src/app/shared/models/charbon.model';
 import { Course } from 'src/app/shared/models/course.model';
+import { User } from 'src/app/shared/models/user.model';
 import { ActionneurService } from 'src/app/shared/services/actionneur.service';
 import { CharbonService } from 'src/app/shared/services/charbon.service';
 import { CourseType } from 'src/app/shared/utils/course-type.model';
@@ -14,12 +16,12 @@ import { CourseType } from 'src/app/shared/utils/course-type.model';
   styleUrls: ['./add-charbon.component.scss'],
 })
 export class AddCharbonComponent implements OnInit {
-  newCharbon!: Charbon;
+  charbonPreview!: Charbon;
   form!: FormGroup;
 
   courseList: Course[] = [];
   courseListForSelectedType: Course[] = [];
-  actionneurList: String[] = [];
+  actionneurList: User[] = [];
 
   courseTypeEnum = CourseType;
 
@@ -31,7 +33,7 @@ export class AddCharbonComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.newCharbon = new Charbon(
+    this.charbonPreview = new Charbon(
       0,
       '',
       CourseType.undefined,
@@ -63,13 +65,13 @@ export class AddCharbonComponent implements OnInit {
     });
 
     this.form.valueChanges.subscribe((data) => {
-      this.newCharbon = new Charbon(
+      this.charbonPreview = new Charbon(
         0,
         data.course,
         data.courseType,
         new Date(data.date),
         data.title,
-        data.actionneurs,
+        data.actionneurs?.map((a: User) => a.username),
         data.description,
         data.replayLink,
         data.resourcesLink
@@ -85,7 +87,17 @@ export class AddCharbonComponent implements OnInit {
   }
 
   addCharbon(): void {
-    this.charbonService.addCharbon(this.newCharbon).subscribe((data) => {
+    let newCharbon: CharbonPostParameters = {
+      title: this.form.get('title')?.value,
+      description: this.form.get('description')?.value,
+      date: new Date(this.form.get('date')?.value),
+      course: this.form.get('course')?.value,
+      actionneurs: this.form.get('actionneurs')?.value.map((a: User) => a.id),
+      replayLink: this.form.get('replayLink')?.value,
+      resourcesLink: this.form.get('resourcesLink')?.value,
+    };
+
+    this.charbonService.addCharbon(newCharbon).subscribe((data) => {
       console.log(data);
     });
   }
--- src/app/shared/services/charbon.service.ts
+++ src/app/shared/services/charbon.service.ts
@@ -1,10 +1,11 @@
+import { CharbonPostParameters } from './../models/charbon-post-parameters.model';
 import { CourseType, getCourseTypeName } from './../utils/course-type.model';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map, tap } from 'rxjs';
 import { Charbon } from 'src/app/shared/models/charbon.model';
 import { getCourseType } from '../utils/course-type.model';
-import { GetCharbonParameter } from '../models/charbon-api-parameter.model';
+import { CharbonGetParameters } from '../models/charbon-get-parameters.model';
 
 @Injectable({
   providedIn: 'root',
@@ -47,7 +48,7 @@ export class CharbonService {
     return params;
   }
 
-  getCharbonList(options: GetCharbonParameter = {}): Observable<Charbon[]> {
+  getCharbonList(options: CharbonGetParameters = {}): Observable<Charbon[]> {
     let params = new HttpParams();
     params = this.setParam(params, 'courses', options.courses);
     params = this.setParam(
@@ -70,13 +71,15 @@ export class CharbonService {
       .pipe(this.processHttpResponses);
   }
 
-  addCharbon(ch: Charbon): Observable<boolean> {
+  addCharbon(data: CharbonPostParameters): Observable<boolean> {
     const formData = new FormData();
-    formData.append('title', ch.title);
-    formData.append('description', ch.description);
-    formData.append('datetime', (ch.date.getTime() / 1000).toString());
-    formData.append('course', ch.course);
-    formData.append('actionneurs', ch.actionners.join(','));
+    formData.append('title', data.title.toString());
+    formData.append('description', data.description.toString());
+    formData.append('datetime', (data.date.getTime() / 1000).toString());
+    formData.append('course', data.course.toString());
+    formData.append('actionneurs', data.actionneurs.join(','));
+    if (data.replayLink) formData.append('replay_link', data.replayLink.toString());
+    if (data.resourcesLink) formData.append('resources_link', data.resourcesLink.toString());
 
     return this.http
       .post<any>(`http://localhost/PLSres/api/charbons`, formData)
