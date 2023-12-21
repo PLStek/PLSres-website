2e5c3c8e99754c04bcd8df553943bb8439014b36
Lucas <hublartlucas@gmail.com>
2023-12-21 16:22:51 +0100

    email utbm

--- src/app/shared/components/form-connection/form-connection.component.ts
+++ src/app/shared/components/form-connection/form-connection.component.ts
@@ -23,11 +23,23 @@ export class FormConnectionComponent implements OnInit {
   login(): void {
     console.log('Email:', this.email);
     console.log('Mot de passe:', this.password);
+    this.checkUTBMEmail(this.email);
   }
 
   register(): void {
     console.log('Nouvel email:', this.newEmail);
     console.log('Nouveau mot de passe:', this.newPassword);
+    this.checkUTBMEmail(this.newEmail);
   }
 
+  private checkUTBMEmail(email: string): void {
+    const utbmEmailRegex: RegExp = /@utbm\.fr$/;
+    const isUtbmEmail: boolean = utbmEmailRegex.test(email);
+
+    if (isUtbmEmail) {
+      console.log('L\'email se termine par @utbm.fr');
+    } else {
+      console.log('L\'email ne se termine pas par @utbm.fr');
+    }
+  }
 }
