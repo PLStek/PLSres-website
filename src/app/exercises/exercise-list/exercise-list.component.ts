4b87bc948f8a2a32611ecae6b28bca98435851ac
Wiqiro <william.imbert@utbm.fr>
2024-01-06 15:39:08 +0100

    feat: exercise topic sort form

--- src/app/exercises/exercise-list/exercise-list.component.ts
+++ src/app/exercises/exercise-list/exercise-list.component.ts
@@ -1,21 +1,67 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Course } from 'src/app/shared/models/course.model';
 import { ExerciseTopic } from 'src/app/shared/models/exercise-topic.model';
+import { CourseService } from 'src/app/shared/services/course.service';
 import { ExerciseTopicService } from 'src/app/shared/services/exercise-topic.service';
+import { CourseType } from 'src/app/shared/utils/course-type.model';
 
 @Component({
   selector: 'app-exercise-list',
   templateUrl: './exercise-list.component.html',
-  styleUrls: ['./exercise-list.component.scss']
+  styleUrls: ['./exercise-list.component.scss'],
 })
 export class ExerciseListComponent implements OnInit {
   @Input() editable = false;
 
   exerciseTopicList!: ExerciseTopic[];
-  difficulty: number = 0;
 
-  constructor(private exerciseTopicService: ExerciseTopicService) {}
+
+  courseList: Course[] = [];
+  courseListForSelectedType: Course[] = [];
+
+  sortForm!: FormGroup;
+
+  courseTypeEnum = CourseType;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private exerciseTopicService: ExerciseTopicService,
+    private courseService: CourseService
+  ) {}
 
   ngOnInit(): void {
+    this.sortForm = this.formBuilder.group({
+      courseType: CourseType.undefined,
+      course: undefined,
+      sort: 'dateDesc',
+    });
+
+    this.courseService.getCourses().subscribe((data) => {
+      this.courseList = data;
+      this.updateCourseList();
+    });
+    
+    this.sortForm.valueChanges.subscribe(() => {
+      this.exerciseTopicList = [];
+      this.fetchExerciseTopics();
+      console.log(this.sortForm.value);
+    });
+    
+    this.sortForm.get('courseType')?.valueChanges.subscribe(() => {
+      this.updateCourseList();
+      this.sortForm.get('course')?.setValue(undefined);
+    });
+
+    
+
+    this.fetchExerciseTopics();
+
+    
+  }
+
+  fetchExerciseTopics(): void {
     this.exerciseTopicService.getExerciseTopicList().subscribe((data) => {
       return (this.exerciseTopicList = data.filter(
         (et) => et.exerciseCount > 0
@@ -23,9 +69,9 @@ export class ExerciseListComponent implements OnInit {
     });
   }
 
-  setDifficulty(difficulty: number): void {
-    this.difficulty = difficulty;
-    console.log(this.difficulty);
+  updateCourseList(): void {
+    this.courseListForSelectedType = this.courseList.filter(
+      (course) => course.type === this.sortForm.get('courseType')?.value
+    );
   }
-
 }
