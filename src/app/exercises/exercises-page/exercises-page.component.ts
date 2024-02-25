9917a8e01ca97f94ea8e5a8eefc72389a67fd882
Wiqiro <william.imbert@utbm.fr>
2024-02-25 15:25:22 +0100

    refactor: charbon & exercise edition enabled for actionneurs

--- src/app/exercises/exercises-page/exercises-page.component.ts
+++ src/app/exercises/exercises-page/exercises-page.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { ExerciseTopic } from 'src/app/shared/models/exercise-topic.model';
-import { ExerciseTopicService } from 'src/app/shared/services/exercise-topic.service';
 import { ExerciseListComponent } from '../exercise-list/exercise-list.component';
 import { BackgroundCardComponent } from '../../shared/components/background-card/background-card.component';
 import { RouterLink } from '@angular/router';
 import { MainButtonComponent } from '../../shared/components/main-button/main-button.component';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
-    selector: 'app-exercises-page',
-    templateUrl: './exercises-page.component.html',
-    styleUrls: ['./exercises-page.component.scss'],
-    standalone: true,
-    imports: [
-        MainButtonComponent,
-        RouterLink,
-        BackgroundCardComponent,
-        ExerciseListComponent,
-    ],
+  selector: 'app-exercises-page',
+  templateUrl: './exercises-page.component.html',
+  styleUrls: ['./exercises-page.component.scss'],
+  standalone: true,
+  imports: [
+    MainButtonComponent,
+    RouterLink,
+    BackgroundCardComponent,
+    ExerciseListComponent,
+  ],
 })
 export class ExercisesPageComponent implements OnInit {
-  constructor() {}
+  actionneur: boolean = false;
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.authService.isActionneur().subscribe((res) => (this.actionneur = res));
+  }
 }
