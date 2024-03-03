011426324014c4d967402f6a4ebc728c4c94532e
Wiqiro <william.imbert@utbm.fr>
2024-03-03 23:35:24 +0100

    feat: announcement and charbon cache

--- src/app/shared/services/announcement.service.ts
+++ src/app/shared/services/announcement.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Announcement } from '../models/announcement.model';
-import { Observable, map } from 'rxjs';
+import { Observable, map, shareReplay } from 'rxjs';
 import { AnnouncementGetParameters } from '../models/announcement-get-parameters.model';
 import { environment } from 'src/environments/environment';
 import { setParam } from '../utils/set-params';
@@ -17,17 +17,16 @@ interface ApiResponse {
   providedIn: 'root',
 })
 export class AnnouncementService {
+  private announcements$?: Observable<Announcement[]>;
+
   constructor(private http: HttpClient) {}
 
-  private processHttpResponses = map((announcements: ApiResponse[]) =>
-    announcements.map(
-      (a: ApiResponse) =>
-        new Announcement(a.id, a.title, a.content, new Date(a.datetime * 1000))
-    )
-  );
+  private transformRes = (a: ApiResponse) =>
+    new Announcement(a.id, a.title, a.content, new Date(a.datetime * 1000));
 
   getAnnouncements(
-    options: AnnouncementGetParameters = {}
+    options: AnnouncementGetParameters = {},
+    useCache: boolean = true
   ): Observable<Announcement[]> {
     let params = new HttpParams();
 
@@ -35,8 +34,15 @@ export class AnnouncementService {
     params = setParam(params, 'offset', options.offset);
     params = setParam(params, 'sort', options.sort);
 
-    return this.http
-      .get<ApiResponse[]>(`${environment.apiURL}/announcements/`, { params })
-      .pipe(this.processHttpResponses);
+    if (!useCache || !this.announcements$) {
+      this.announcements$ = this.http
+        .get<ApiResponse[]>(`${environment.apiURL}/announcements/`, { params })
+        .pipe(
+          map((announcements) => announcements.map(this.transformRes)),
+          shareReplay(1)
+        );
+    }
+
+    return this.announcements$;
   }
 }
